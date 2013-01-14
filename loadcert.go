a7a7f932b1e1fdf8e1da80fdfd4f7cd1f4e52a46
Matt Banack <matt@banack.net>
2013-01-13 22:47:54 -0700

    take ASN1 unmarshal testing out altogether

--- loadcert.go
+++ loadcert.go
@@ -47,77 +47,9 @@ func tryTLSParse(crtFile string, keyFile string) {
         }
 }
 
-func tryASN1UnmarshalToX509(crtFile string) {
-        fmt.Println(">> tryASN1UnmarshalToX509",crtFile)
-        fi, err := os.Open(crtFile)
-        if err != nil {
-                panic("Error opening " + crtFile)
-        }
-        pemBytes := make([]byte, 1024)
-        n, err := fi.Read(pemBytes)
-        if n >= 1024 {
-                panic("cert bigger than buffer")
-        }
-        if err != io.EOF && err != nil {
-                fmt.Println(err)
-                panic("non-EOF error in cert read")
-        }
-
-        block, pemrest := pem.Decode(pemBytes[:n])
-        if len(pemrest) > 0 {
-                fmt.Println("pem.Decode had trailing",pemrest)
-        }
-        var cert x509.Certificate
-        rest, err := asn1.Unmarshal(block.Bytes, &cert)
-        if err != nil {
-                fmt.Println("err in asn1.Unmarshal",err)
-        } else {
-                fmt.Println("<< Success!")
-        }
-        if len(rest) > 0 {
-                fmt.Println("rest from asn1.Unmarshal",rest)
-        }
-}
-
-func tryASN1UnmarshalToTLS(crtFile string) {
-        fmt.Println(">> tryASN1UnmarshalToTLS",crtFile)
-        fi, err := os.Open(crtFile)
-        if err != nil {
-                panic("Error opening " + crtFile)
-        }
-        pemBytes := make([]byte, 1024)
-        n, err := fi.Read(pemBytes)
-        if n >= 1024 {
-                panic("cert bigger than buffer")
-        }
-        if err != io.EOF && err != nil {
-                fmt.Println(err)
-                panic("non-EOF error in cert read")
-        }
-
-        block, pemrest := pem.Decode(pemBytes[:n])
-        if len(pemrest) > 0 {
-                fmt.Println("pem.Decode had trailing",pemrest)
-        }
-        var cert tls.Certificate
-        rest, err := asn1.Unmarshal(block.Bytes, &cert)
-        if err != nil {
-                fmt.Println("err in asn1.Unmarshal",err)
-        } else {
-                fmt.Println("<< Success!")
-        }
-        if len(rest) > 0 {
-                fmt.Println("rest from asn1.Unmarshal",rest)
-        }
-}
-
 func main() {
         tryTLSParse("client.crt", "client.key");
         tryTLSParse("client.clientAuth.crt", "client.key");
         tryX509Parse("client.crt");
         tryX509Parse("client.clientAuth.crt");
-        //tryASN1UnmarshalToX509("client.crt");
-        //tryASN1UnmarshalToX509("client.clientAuth.crt");
-        //tryASN1UnmarshalToTLS("client.crt");
-        //tryASN1UnmarshalToTLS("client.clientAuth.crt");
 }
